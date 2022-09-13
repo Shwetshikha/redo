c56907bdd837a2f4fb817bd7b101aac7261e7663
Shwet Shikha <Shwet@shorthillstech.com>
2022-09-13 10:52:07 +0530

    Redo page

---
+++ product.js
@@ -0,0 +1,7 @@
+const buttonElement = document.getElementsByClassName('cart');
+
+function openProduct(){
+    <a href="product.html"></a>
+}
+
+buttonElement.addEventListener('click', openProduct);
